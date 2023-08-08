ac28f5188b824492e064b8f9c71a4482d2da5e9d
Sam Nguyen <nguyenlehanguyen102@gmail.com>
2023-08-08 18:56:55 -0400

    Set up routes and page templates

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,24 @@
-import {AppRoutes} from './Routes';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <AppRoutes/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
