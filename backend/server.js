8af30477bd3219098e29b34e8683a8568f0c81f6
maocran <adwoa@whatscookin.com>
2023-08-14 18:58:42 -0400

    log in authorization with firebase

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 let port = 5000;
-let hostname = "localhost";
+let hostname = 'localhost';
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo"] })
-})
+app.get('/api', (req, res) => {
+  res.json({ users: ['userOne', 'userTwo'] });
+});
 
 app.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}`);
-})
+  console.log(`http://${hostname}:${port}`);
+});
--- front/src/pages/LoginPage/LoginPage.js
+++ front/src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,8 @@
-
 import React, { useState } from 'react';
 import './LoginPage.css';
-
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 /*
 export const LoginPage = () => {
@@ -15,47 +16,85 @@ export const LoginPage = () => {
 */
 
 function LoginPage() {
-    const [loginFormActive, setLoginFormActive] = useState(false);
+  const [loginFormActive, setLoginFormActive] = useState(false);
 
-    const toggleLoginForm = () => {
-        setLoginFormActive(true);
-    };
+  const toggleLoginForm = () => {
+    setLoginFormActive(true);
+  };
 
-    const goBack = () => {
-        setLoginFormActive(false);
-    };
+  const goBack = () => {
+    setLoginFormActive(false);
+  };
 
-    return (
-        <div>
-            <img src="https://media.giphy.com/media/h7uTwqEHysbd2lhyDP/giphy.gif" />
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-            <div className="card">
-                <div className="title">Harmony Plate</div>
-                <div className="button-group" id="button-group">
-                  <a className={`button signup ${loginFormActive ? 'hidden' : ''}`}>
-                    Sign Up
-                  </a>
-                  <a
-                  className={`button login ${loginFormActive ? 'hidden' : ''}`}
-                  id="initialLogin"
-                  onClick={toggleLoginForm}
-                  >
-                    Login
-                  </a>
-                </div>
-                <div className={`login-form ${loginFormActive ? 'active' : ''}`} id="login-form">
-                    <input type="email" placeholder="Email" id="email" />
-                    <br />
-                    <input type="password" placeholder="Password" id="password" />
-                    <br />
-                    <button className="back-button" onClick={goBack}>
-                        &#8592;
-                    </button>
-                    <button id="login-button">Login</button>
-                </div>
-            </div>
+  const onLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate('/create-profile');
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <div>
+      <img src="https://media.giphy.com/media/h7uTwqEHysbd2lhyDP/giphy.gif" />
+
+      <div className="card">
+        <div className="title">Harmony Plate</div>
+        <div className="button-group" id="button-group">
+          <a className={`button signup ${loginFormActive ? 'hidden' : ''}`}>
+            Sign Up
+          </a>
+          <a
+            className={`button login ${loginFormActive ? 'hidden' : ''}`}
+            id="initialLogin"
+            onClick={toggleLoginForm}
+          >
+            Login
+          </a>
         </div>
-    );
+        <div
+          className={`login-form ${loginFormActive ? 'active' : ''}`}
+          id="login-form"
+        >
+          <input
+            type="email"
+            placeholder="Email address"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button className="back-button" onClick={goBack}>
+            &#8592;
+          </button>
+          <button id="login-button" onClick={onLogin}>
+            Login
+          </button>
+        </div>
+        <p className="text-sm text-white text-center">
+          No account yet? <NavLink to="/signup">Sign up</NavLink>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
