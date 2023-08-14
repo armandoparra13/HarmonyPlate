e0f779d2dc1a50f776a036188cc746068207bb92
ajocr <adwoa@whatscookin.com>
2023-08-14 19:44:34 -0400

    created profile creation js file to test

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,15 +1,16 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import { MatchingPage } from './pages/MatchingPage/MatchingPage';
+import CreateProfilePage from './pages/ProfileCreationPage/CreateProfilePage';
 
 export const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage />}/>
-                <Route path="/matching" element={<MatchingPage />}/>
-            </Routes>
-        </Router>
-
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/matching" element={<MatchingPage />} />
+        <Route path="/create-profile" element={<CreateProfilePage />} />
+      </Routes>
+    </Router>
+  );
+};
---
+++ front/src/pages/ProfileCreationPage/CreateProfilePage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+/*
+export const LoginPage = () => {
+  return (
+    <div className="LoginPage">
+        <h1>This is Log-in Page!</h1>
+    </div>
+  );
+}
+
+*/
+
+function CreateProfilePage() {
+  return <div>hi- add profile frontend</div>;
+}
+
+export default CreateProfilePage;
