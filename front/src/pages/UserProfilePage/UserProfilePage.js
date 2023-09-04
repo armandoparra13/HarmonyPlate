cf83d9f7e8ac88c7adb0f9440d7b789cc292c06c
Sam Nguyen <nguyenlehanguyen102@gmail.com>
2023-09-04 11:58:18 -0400

    add styling

--- front/src/pages/UserProfilePage/UserProfilePage.js
+++ front/src/pages/UserProfilePage/UserProfilePage.js
@@ -12,65 +12,90 @@ function UserProfilePage() {
     useEffect(() => {
         // Function to fetch user data
         const fetchUserData = async () => {
-        try {
-            const response = await axios.get("/auth/getUserProfile", {
-            headers: {
-                authorization: currentUser.accessToken, 
-            },
-            });
+            try {
+                const response = await axios.get("/auth/getUserProfile", {
+                    headers: {
+                        authorization: currentUser.accessToken,
+                    },
+                });
 
-            setUserData(response.data);
-            console.log(userData);
+                setUserData(response.data);
 
-            // Fetch favorite dish data
-            favoriteDish(response.data.food.favoriteFood);
-        } catch (error) {
-            console.error("Error fetching user data:", error);
-        }
+                // Fetch favorite dish data
+                favoriteDish(response.data.food.favoriteFood);
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
         };
-        
+
         const favoriteDish = async (dishId) => {
             fetch(`/auth/recipe/${dishId}`)
-            .then((response) => {
-                return response.json();
-            })
-            .then((dishData) => {
-                console.log("Favorite dish data:", dishData);
-                setFavoriteFoodImage(dishData.image);
-                setFavoriteFoodTitle(dishData.title);
-                // Do something with the favorite dish data
-            })
-            .catch((error) => {
-                console.error('Error fetching favorite dish:', error);
-            });
+                .then((response) => {
+                    return response.json();
+                })
+                .then((dishData) => {
+                    setFavoriteFoodImage(dishData.image);
+                    setFavoriteFoodTitle(dishData.title);
+                    // Do something with the favorite dish data
+                })
+                .catch((error) => {
+                    console.error('Error fetching favorite dish:', error);
+                });
         }
 
         fetchUserData();
-    }, []);
-        
+    }, [currentUser.accessToken]);
+
     return (
-        <div className='grid-containter'>
-            <div className="grid-item">1</div>
-            <h1>User Profile</h1>
-                
-                <div className="grid-item">2</div>
-            {userData ? (
-                <div>
-                <img src="/uploads/abcdef/Funny-Cat-Hidden.jpg" alt="upload" ></img>
-                <p>Username: {userData.username}</p>
-                <p>Description: {userData.description}</p>
-                <p>Email: {userData.email}</p>
-                <p>DOB: {userData.dateOfBirth}</p>
-                <p>Gender: {userData.gender}</p>
-                <p>Diet: {userData.food.diet}</p>
-                <p>Favorite dish: {favoriteFoodTitle}</p>
-                <img src={favoriteFoodImage}></img>
-                {/* Render other user data */}
-                </div>
-            ) : (
-                <p>Loading user data...</p>
-            )}
+        <div className='grid-container'>
+            <div className="left-column">
+                {userData ? (
+                    <div>
+                        <div className="first-row">
+                            <img src="/uploads/abcdef/world-s-cutest-kitten.jpg" alt="upload" className="user-image" />
+                        </div>
+                        <div className="second-row">
+                            <h2>{userData.username}</h2>
+                            <div className="input-group">
+                                <label>Description</label>
+                                <input className="custom-input" disabled value={userData.description} />
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <p>Loading user data...</p>
+                )}
+            </div>
+            <div className="right-column">
+                {userData ? (
+                    <div>
+                        <div className="input-group">
+                            <label>Email</label>
+                            <input className="custom-input" disabled value={userData.email} />
+                        </div>
+                        <div className="input-group">
+                            <label>Date of Birth</label>
+                            <input className="custom-input" disabled value={userData.dateOfBirth} />
+                        </div>
+                        <div className="input-group">
+                            <label>Gender</label>
+                            <input className="custom-input" disabled value={userData.gender}>
+                            </input>
+                        </div>
+                        <div className="input-group">
+                            <label>Diet</label>
+                            <input className="custom-input" disabled value={userData.food.diet} />
+                        </div>
+                        <div className="input-group">
+                            <label>Favorite Dish</label>
+                            <input className="custom-input" disabled value={favoriteFoodTitle} />
+                        </div>
+                        <img src={favoriteFoodImage} alt="Favorite Dish" />                       
+                    </div>
+                ) : null}
+            </div>
         </div>
     );
-};
+}
+
 export default UserProfilePage;
