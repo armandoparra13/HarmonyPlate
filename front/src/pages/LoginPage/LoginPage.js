109a0804a5e1a0affe6573aead3f8d51c35a7119
Armando Parra <ajp434@drexel.edu>
2023-09-04 21:24:01 -0400

    Quick fix

--- front/src/pages/LoginPage/LoginPage.js
+++ front/src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './LoginPage.css';;
+import './LoginPage.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../Auth'
 
